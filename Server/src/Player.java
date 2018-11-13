65d838ca16d8464229b3c3076c7a683dbc46e339
KarolinaPGol <31969537+KarolinaPGol@users.noreply.github.com>
2018-11-13 21:36:54 +0100

    Server updated

--- Server/src/Player.java
+++ Server/src/Player.java
@@ -3,19 +3,19 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+// a separate Player class
 public class Player {
-	
-	private Socket socket;	
+
+	private Socket socket;
 	private String wantToStartGame;
-	
+
 	public String username;
 	public int tool;
 	public DataInputStream fromClient;
 	public DataOutputStream toClient;
 	public String playAgain;
-	
-	
+
+
 	void setSocket(ServerSocket serverSocket){
 		try {
 			socket = serverSocket.accept();
@@ -27,7 +27,7 @@ public class Player {
 	Socket getSocket(){
 		return socket;
 	}
-	
+
 	void setDataStreams(){
 		try {
 			fromClient = new DataInputStream(socket.getInputStream());
@@ -37,45 +37,50 @@ public class Player {
 			e.printStackTrace();
 		}
 	}
-	
+
 	void askWantToStartGame(){
+
+		//using a try/catch exception handling
 		try {
-			 wantToStartGame = fromClient.readUTF();
+			//receives the players' answers on whether they want to start a game
+			wantToStartGame = fromClient.readUTF();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
 	String getWantToStartGame(){
 		return wantToStartGame;
 	}
-	
+
 	void setUsername(){
 		try {
+			//reads what the users set as their username
 			username = fromClient.readUTF();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	void setTool(){
 		try {
+			//reads what tools each client chooses
 			tool = fromClient.readInt();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	void setPlayAgain(){
 		try {
+			//reads users' choice on whether they want to continue playing the game
 			playAgain = fromClient.readUTF();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 }
--- Server/src/Server.java
+++ Server/src/Server.java
@@ -4,230 +4,249 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+
+//creating the public class for the server code used to accept the sockets and players
 public class Server{
 
-public static void main(String[] args){
-		
-		
-		
+
+	public static void main(String[] args){
+
+
+		//creating a new Thread
 		new Thread( () -> {
+
+			//using the try/catch exception handling
 			try {
+
+				//printing out a message to show that the server is now running
 				System.out.println("Server Started");
 				//----------------------SOCKET SET UP----------------------
-				 
-				
+
+				// Listen for a connection request
 				ServerSocket serverSocket = new ServerSocket(9900);
-				 // Listen for a connection request
-				
+
+
+				//Creating a Players array to store all players
+				//The length of the array is equals to 3 since we only have 3 clients to connect to the server
+
 				Player[] players = new Player[3];
+				//for loop used to set the current position in the array to a new player
 				for(int i=0; i<players.length; i++){
 					players[i] = new Player();
 				}
-				
-				 //--
+
+				//for loop for filling in the Player array with players, assigns the socket to specific clients/players
 				for(int i=0; i<players.length; i++){
 					players[i].setSocket(serverSocket);
+					//Prints out a message whenever every client is connected
 					System.out.println("Client " + i + " is connected");
 				}
-				 
-			     new Thread(new HandleSession(players)).start();
+				//creates a new thread for the connected client
+				new Thread(new HandleSession(players)).start();
 			}catch(IOException ex) {
-			} 
+			}
+			//starts the thread
 		}).start();
-	
-	
-		} //main
+
+
+	} //end of server class
 }
 
-class HandleSession implements Runnable{
-	
-	private Player[] players;	
-	private boolean playAgain;	
 
+//HandleSession class starts and implements the Runnable interface
+//Runnable is needed to run threads that causes objects' run method to be called in the specific thread
+class HandleSession implements Runnable{
+
+	//initialize the array in which we store the players
+	private Player[] players;
+	//initializing a variable that keeps track of whether a game is being played or on the verge to start
+	private boolean playAgain;
+	//assigning the array from the server to the array in the HandleSession
+	//to tell which specific players want to play
 	public HandleSession(Player[] players){
-		this.players = players;		
+		this.players = players;
 	}
-	
+
 	@Override
 	public void run() {
-	
+
 		try{
-		
-			
+
+			//prints out a message whenever all the players are connected
 			System.out.println("All clients connected");
 			Scanner qScan = new Scanner(System.in);
-			
+
 			// Create data input and output streams
 			for(int i=0; i<players.length;  i++){
 				players[i].setDataStreams();
 			}
-		 
-		//--------------------------READING INPUTS--------------------------
-		 
-			
+
+			//--------------------------READING INPUTS--------------------------
+
+			//asks each player if they want to start the game
 			for(int i=0; i<players.length;  i++){
 				players[i].askWantToStartGame();
 				System.out.println("Client " + i + " wants to start game: " + players[i].getWantToStartGame());
 				//players[i].toClient.writeUTF("You are connected to server");
 			}
 
+			//asks every player to submit a username
 			for(int i=0; i<players.length;  i++){
 				players[i].setUsername();
 				System.out.println("Client " + i + " username: " + players[i].username );
 			}
 
 
-		 players[0].toClient.writeUTF(players[1].username);
-		 players[0].toClient.writeUTF(players[2].username);
-		 
-		 players[1].toClient.writeUTF(players[0].username);
-		 players[1].toClient.writeUTF(players[2].username);
-		 
-		 players[2].toClient.writeUTF(players[0].username);
-		 players[2].toClient.writeUTF(players[1].username);
-		 
+			//each player selects their username
+			players[0].toClient.writeUTF(players[1].username);
+			players[0].toClient.writeUTF(players[2].username);
 
-		do {
-		 //get tool
-		 
-		for(int i=0; i<players.length; i++){
-			players[i].setTool();
-			System.out.println(players[i].username + "'s tool: " + players[i].tool);
-		}
-			
-		players[0].toClient.writeInt(players[1].tool);
-		players[0].toClient.writeInt(players[2].tool);
-		
-		players[1].toClient.writeInt(players[0].tool);
-		players[1].toClient.writeInt(players[2].tool);
-		
-		players[2].toClient.writeInt(players[0].tool);
-		players[2].toClient.writeInt(players[1].tool);
-		
-	     
-	     //everyone tie
-	     if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 0) ||
-		 	    	(players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 1) ||
-		 	    	(players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 2) ||
-		 	        (players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 1) ||
-		 	        (players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 2) ||
-		 	        (players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 0) ||
-		 	        (players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 2) ||
-		 	        (players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 0) ||
-		 	        (players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 1) ) {
-		 	  
-		 	    	players[0].toClient.writeUTF("All players are tied");
-		 	    	players[1].toClient.writeUTF("All players are tied");
-		 	    	players[2].toClient.writeUTF("All players are tied");
-		 	 
-	
-		 	     }
-		 	     
-	     //PLAYER 1 WIN
-		 	     if((players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 2) ||
-		 	    	(players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 0) ||
-		 	    	(players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 1) ) {
-		 	    	
-		 	    	players[0].toClient.writeUTF(players[0].username + "wins");
-		 	    	players[1].toClient.writeUTF(players[0].username + "wins");
-		 	    	players[2].toClient.writeUTF(players[0].username + "wins");
-		 	     }
-		 	     
-		 	     //PLAYER 2 WIN
-		 	     if((players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 0) ||
-		 	    	(players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 1) ||
-		 	    	(players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 2) ) {
-		 	    	
-		 	    	players[0].toClient.writeUTF(players[1].username + "wins");
-		 	    	players[1].toClient.writeUTF(players[1].username + "wins");
-		 	    	players[2].toClient.writeUTF(players[1].username + "wins");
-		 	     }
-		 	     
-		 	     //PLAYER 3 WIN
-		 	     if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 1) ||
-		 	 	    (players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 2) ||
-		 	 	    (players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 0) ) {
-		 	 	    	
-		 	    	players[0].toClient.writeUTF(players[2].username + "wins");
-		 	    	players[1].toClient.writeUTF(players[2].username + "wins");
-		 	    	players[2].toClient.writeUTF(players[2].username + "wins");
-		 	 	 }
-		 	 	     
-		 	     //PLAYER 2 & PLAYER 3 TIE
-		 	     if((players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 1) ||
-		 	 	 	(players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 2) ||
-		 	 	 	(players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 0) ) {
-		 	 	 	    	
-		 	    	players[0].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
-		 	    	players[1].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
-		 	    	players[2].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
-		 	 	 	 }
-		 	     
-		 	     //PLAYER 1 & PLAYER 2 TIE
-		 	     if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 2) ||
-		 	 	 	(players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 0) ||
-		 	 	 	(players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 1) ) {
-		 	 	 	    	
-		 	    	players[0].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
-		 	    	players[1].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
-		 	    	players[2].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
-		 	 	 	 }
-		 	     
-		 	     //PLAYER 1 & PLAYER 3 TIE
-		 	     if((players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 0) ||
-		 	 	 	(players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 1) ||
-		 	 	 	(players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 2) ) {
-		 	 	 	    	
-		 	    	players[0].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
-		 	    	players[1].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
-		 	    	players[2].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
-		 	 	 	 }
-		
-		 	     
-		for(int i=0; i<players.length; i++){
-			players[i].setPlayAgain();
-			System.out.println("player "+i+ " wants to play again: "+ players[i].playAgain);
-		}
-		
-	    
-	    if(players[0].playAgain.equals("y") && players[1].playAgain.equals("y") && players[2].playAgain.equals("y") ) {
-	    	
-	    	playAgain = true;
-	    	
-	    }
-	    else if(players[0].playAgain.equals("n") || players[1].playAgain.equals("n")  || players[2].playAgain.equals("n") ){
-	    	playAgain = false;
-	    }
-	    
-	    
-	   if(playAgain == true){
-	   players[0].toClient.writeUTF("Everybody wants to play again! Lets do it");
-	   players[1].toClient.writeUTF("Everybody wants to play again! Lets do it");
-	   players[2].toClient.writeUTF("Everybody wants to play again! Lets do it");
-	  
-	   players[0].toClient.writeBoolean(playAgain);
-	   players[1].toClient.writeBoolean(playAgain);
-	   players[2].toClient.writeBoolean(playAgain);
-	   }
-	   
-	   if(playAgain == false) {
-		   players[0].toClient.writeUTF("A player selected no to play again, ending game");
-		   players[1].toClient.writeUTF("A player selected no to play again, ending game");
-		   players[2].toClient.writeUTF("A player selected no to play again, ending game");
-		  
-		   players[0].toClient.writeBoolean(playAgain);
-		   players[1].toClient.writeBoolean(playAgain);
-		   players[2].toClient.writeBoolean(playAgain);
-	   }
-	   
-		}while(playAgain == true);
-		System.exit(0);
+			players[1].toClient.writeUTF(players[0].username);
+			players[1].toClient.writeUTF(players[2].username);
+
+			players[2].toClient.writeUTF(players[0].username);
+			players[2].toClient.writeUTF(players[1].username);
+
+
+			do {
+				//get tool
+
+				for(int i=0; i<players.length; i++){
+					players[i].setTool();
+					System.out.println(players[i].username + "'s tool: " + players[i].tool);
+				}
+				//each player selects their tool
+				players[0].toClient.writeInt(players[1].tool);
+				players[0].toClient.writeInt(players[2].tool);
+
+				players[1].toClient.writeInt(players[0].tool);
+				players[1].toClient.writeInt(players[2].tool);
+
+				players[2].toClient.writeInt(players[0].tool);
+				players[2].toClient.writeInt(players[1].tool);
+
+
+				//the combination of tools selected by players that would result in a tie
+				if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 0) ||
+						(players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 1) ||
+						(players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 2) ||
+						(players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 1) ||
+						(players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 2) ||
+						(players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 0) ||
+						(players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 2) ||
+						(players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 0) ||
+						(players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 1) ) {
+
+					players[0].toClient.writeUTF("All players are tied");
+					players[1].toClient.writeUTF("All players are tied");
+					players[2].toClient.writeUTF("All players are tied");
+
+
+				}
+
+				//the combination of tools selected by players that would result in player 1 winning
+				if((players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 2) ||
+						(players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 0) ||
+						(players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 1) ) {
+
+					players[0].toClient.writeUTF(players[0].username + "wins");
+					players[1].toClient.writeUTF(players[0].username + "wins");
+					players[2].toClient.writeUTF(players[0].username + "wins");
+				}
+
+				//the combination of tools selected by players that would result in player 2 winning
+				if((players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 0) ||
+						(players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 1) ||
+						(players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 2) ) {
+
+					players[0].toClient.writeUTF(players[1].username + "wins");
+					players[1].toClient.writeUTF(players[1].username + "wins");
+					players[2].toClient.writeUTF(players[1].username + "wins");
+				}
+
+				//the combination of tools selected by players that would result in player 3 winning
+				if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 1) ||
+						(players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 2) ||
+						(players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 0) ) {
+
+					players[0].toClient.writeUTF(players[2].username + "wins");
+					players[1].toClient.writeUTF(players[2].username + "wins");
+					players[2].toClient.writeUTF(players[2].username + "wins");
+				}
+
+				//PLAYER 2 & PLAYER 3 TIE
+				if((players[0].tool == 0 && players[1].tool == 1 && players[2].tool == 1) ||
+						(players[0].tool == 1 && players[1].tool == 2 && players[2].tool == 2) ||
+						(players[0].tool == 2 && players[1].tool == 0 && players[2].tool == 0) ) {
+
+					players[0].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
+					players[1].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
+					players[2].toClient.writeUTF(players[1].username + "&" + players[2].username + "are tied");
+				}
+
+				//PLAYER 1 & PLAYER 2 TIE
+				if((players[0].tool == 0 && players[1].tool == 0 && players[2].tool == 2) ||
+						(players[0].tool == 1 && players[1].tool == 1 && players[2].tool == 0) ||
+						(players[0].tool == 2 && players[1].tool == 2 && players[2].tool == 1) ) {
+
+					players[0].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
+					players[1].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
+					players[2].toClient.writeUTF(players[1].username + "&" + players[0].username + "are tied");
+				}
+
+				//PLAYER 1 & PLAYER 3 TIE
+				if((players[0].tool == 0 && players[1].tool == 2 && players[2].tool == 0) ||
+						(players[0].tool == 1 && players[1].tool == 0 && players[2].tool == 1) ||
+						(players[0].tool == 2 && players[1].tool == 1 && players[2].tool == 2) ) {
+
+					players[0].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
+					players[1].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
+					players[2].toClient.writeUTF(players[0].username + "&" + players[2].username + "are tied");
+				}
+
+				//asks each of the players if they want to play again
+				for(int i=0; i<players.length; i++){
+					players[i].setPlayAgain();
+					System.out.println("player "+i+ " wants to play again: "+ players[i].playAgain);
+				}
+
+				//the condition in which players start a new game
+				if(players[0].playAgain.equals("y") && players[1].playAgain.equals("y") && players[2].playAgain.equals("y") ) {
+
+					playAgain = true;
+
+				}
+
+				//the condition in which players terminate the game
+				else if(players[0].playAgain.equals("n") || players[1].playAgain.equals("n")  || players[2].playAgain.equals("n") ){
+					playAgain = false;
+				}
+
+
+				if(playAgain == true){ //if everyone wants to start a new game, this message gets printed to every player
+					players[0].toClient.writeUTF("Everybody wants to play again! Lets do it");
+					players[1].toClient.writeUTF("Everybody wants to play again! Lets do it");
+					players[2].toClient.writeUTF("Everybody wants to play again! Lets do it");
+
+					players[0].toClient.writeBoolean(playAgain);
+					players[1].toClient.writeBoolean(playAgain);
+					players[2].toClient.writeBoolean(playAgain);
+				}
+
+				if(playAgain == false) { //if everyone wants to terminate the game, this message gets printed to every player
+					players[0].toClient.writeUTF("A player selected no to play again, ending game");
+					players[1].toClient.writeUTF("A player selected no to play again, ending game");
+					players[2].toClient.writeUTF("A player selected no to play again, ending game");
+
+					players[0].toClient.writeBoolean(playAgain);
+					players[1].toClient.writeBoolean(playAgain);
+					players[2].toClient.writeBoolean(playAgain);
+				}
+
+			}while(playAgain == true);
+			System.exit(0);
 		}catch(IOException ex) {
-		} 
+		}
 	}
-	
+
 }
-
-
-
-
